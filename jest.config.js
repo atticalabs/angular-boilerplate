f644fc556dd97a5411b1f71e563f82eab20cb5c5
Federico Silva <federicosilva09@gmail.com>
2020-02-13 19:25:27 -0300

    fix all issues ＼（＾ ＾）／

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,7 @@
 "use strict";
-const { pathsToModuleNameMapper } = require("ts-jest/utils");
 const { compilerOptions } = require("./tsconfig.json");
 
 module.exports = {
   preset: "jest-preset-angular",
-  "setupFilesAfterEnv": ["<rootDir>/setupJest.ts"],
-  verbose: true,
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
-    prefix: '<rootDir>/'
-  })
+  verbose: true
 };
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,36 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-import {AlertMessageComponent} from './alert-message/alert-message.component';
-import {PaginationComponent} from './pagination/pagination.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AppRoutingModule} from './app-routing.module';
-import {CardModule} from './card/card.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {BrowserModule} from '@angular/platform-browser';
-import {CardsService} from './cards.service';
-import {APP_BASE_HREF} from '@angular/common';
+import { TestBed, async } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppComponent } from "./app.component";
+import { AlertMessageComponent } from "./alert-message/alert-message.component";
+import { PaginationComponent } from "./pagination/pagination.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./app-routing.module";
+import { CardModule } from "./card/card.component";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { BrowserModule } from "@angular/platform-browser";
+import { CardsService } from "./cards.service";
+import { APP_BASE_HREF } from "@angular/common";
+import { Router } from "@angular/router";
+class RouterStub {
+  url = "";
+  navigate(commands: any[], extras?: any) {}
+}
 
-
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async(() => {
+    RouterTestingModule.withRoutes([
+      { path: "example", component: AppComponent }
+    ]);
+
     TestBed.configureTestingModule({
       imports: [
         BrowserModule,
         RouterTestingModule,
         BrowserAnimationsModule,
-        AppRoutingModule,
         CardModule,
         MatFormFieldModule,
         MatDatepickerModule,
@@ -31,31 +38,33 @@ describe('AppComponent', () => {
         MatInputModule,
         MatSelectModule
       ],
-      providers: [CardsService, {provide: APP_BASE_HREF, useValue : '/' }],
-      declarations: [
-        AppComponent,
-        AlertMessageComponent,
-        PaginationComponent
+      providers: [
+        CardsService,
+        { provide: APP_BASE_HREF, useValue: "/" },
+        { provide: Router, useClass: RouterStub }
       ],
+      declarations: [AppComponent, AlertMessageComponent, PaginationComponent]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  xit(`should have as title 'angular-boilerplate'`, () => {
+  it(`should have as title 'angular-boilerplate'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-boilerplate');
+    expect(app.title).toEqual("angular-boilerplate");
   });
 
-  xit('should render title', () => {
+  it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-boilerplate app is running!');
+    expect(compiled.querySelector("h1").textContent).toContain(
+      "Angular Boilerplate"
+    );
   });
 });
